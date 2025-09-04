9f8d5864793f145b6b96bcffcdb1fb6f90f75491
MustafaNahas <99922508+MustafaNahas@users.noreply.github.com>
2025-09-04 10:56:53 +0200

    Update App.tsx
    delete
                      <Route path="/Products" element={<Products />} />
                      <Route path="/about" element={<About  />} />
                      <Route path="/Login" element={<Login/>} />
                      <Route path="/DashboardPage" element={<ProtectedRoute><DashboardPage/></ProtectedRoute>} />
                      <Route path="/Settings" element={<ProtectedRoute><Settings/></ProtectedRoute>} />

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,8 @@
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import { AuthProvider } from "./components/AuthContext";
 import Navbar from "./components/Navbar";
-import About  from "./pages/About";
-import Products from "./components/ProductTable.tsx";
 
-import DashboardPage from "./pages/DashboardPage.tsx";
-import ProtectedRoute from "./components/ProtectedRoute.tsx";
-import Login from "./pages/Login.tsx";
-import Settings from "./pages/Settings.tsx";
 
 function App() {
 
@@ -17,11 +11,7 @@ function App() {
           <AuthProvider>
               <Navbar />
               <Routes>
-                  <Route path="/Products" element={<Products />} />
-                  <Route path="/about" element={<About  />} />
-                  <Route path="/Login" element={<Login/>} />
-                  <Route path="/DashboardPage" element={<ProtectedRoute><DashboardPage/></ProtectedRoute>} />
-                  <Route path="/Settings" element={<ProtectedRoute><Settings/></ProtectedRoute>} />
+
               </Routes>
           </AuthProvider>
       </BrowserRouter>
